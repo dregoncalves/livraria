3bb5d6d306a5aba7820ecf9305770c97023a3ff0
Andre Gonçalves <dregoncalves22@gmail.com>
2024-04-08 11:28:55 -0300

    Update

---
+++ src/Livro.java
@@ -0,0 +1,63 @@
+public class Livro {
+    private String nome;
+    private String descricao;
+    private double valor;
+    private String isbn;
+    private Autor autor;
+
+    public Livro(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Livro() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public void mostrarDetalhes() {
+        System.out.println("Mostrando detalhes do livro ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("--");
+    }
+}
